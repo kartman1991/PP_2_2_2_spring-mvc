2d01cdedfce8cfc5e8b33202ffe04149d7729a71
igoruskorcev <cybix@yandex.ru>
2022-08-24 23:26:04 +0300

    a

--- src/main/java/web/service/CarsService.java
+++ src/main/java/web/service/CarsService.java
@@ -4,15 +4,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CarsService implements CarService {
+    List<String> carsList;
 
-    List<String> carsList = new ArrayList<>();
-
-    public List<String> getCarsList() {
+    {
+        carsList = new ArrayList<>();
         carsList.add("VW G8");
         carsList.add("E46");
         carsList.add("W213");
         carsList.add("Giulia");
         carsList.add("Teana");
+    }
+
+    public List<String> getCarsList() {
         return carsList;
     }
     public Integer getCount(Integer count) {
